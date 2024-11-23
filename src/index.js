62c0efc2bc7f9d864005f29108863fef859c7a0d
fath11 <fathimkamal@gmail.com>
2024-11-23 09:25:52 +0800

    allow selecting the current sprite in attachments for clones

--- src/index.js
+++ src/index.js
@@ -1395,20 +1395,11 @@ If I ever decide to release this extension on the gallery, this will be replaced
     getSprites() {
       let spriteNames = [];
       const targets = runtime.targets;
-      const myself = runtime.getEditingTarget().sprite.name;
       for (let index = 1; index < targets.length; index++) {
         const curTarget = targets[index].sprite;
-        let display = curTarget.name;
-        if (myself === curTarget.name) {
-          continue;
-          /*display = Scratch.translate({
-            default: "myself",
-            description: "Item in a dropdown that refers to the current sprite",
-          });*/
-        }
         if (targets[index].isOriginal) {
           const jsonOBJ = {
-            text: display,
+            text: curTarget.name,
             value: curTarget.name,
           };
           spriteNames.push(jsonOBJ);
@@ -1977,6 +1968,8 @@ If I ever decide to release this extension on the gallery, this will be replaced
       if (!targetObj) return;
       const dr = renderer._allDrawables[util.target.drawableID];
       const targetDr = renderer._allDrawables[targetObj.drawableID];
+      if (dr === targetDr) return;
+
       dr[ATTACHED_TO] = targetDr;
       this.updateAttachment(dr);
     }
@@ -1995,6 +1988,8 @@ If I ever decide to release this extension on the gallery, this will be replaced
         }
       }
       if (targetDr === undefined) return;
+      if (dr === targetDr) return;
+      
       dr[ATTACHED_TO] = targetDr;
       this.updateAttachment(dr);
     }
